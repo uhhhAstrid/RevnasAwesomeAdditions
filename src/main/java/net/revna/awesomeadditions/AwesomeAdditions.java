df518a74cf727df3ac883c40f311a4a6e137ab85
sillycaterpillar <revnacodes@gmail.com>
2025-06-29 13:37:33 -0400

    first items textured and added :)

--- src/main/java/net/revna/awesomeadditions/AwesomeAdditions.java
+++ src/main/java/net/revna/awesomeadditions/AwesomeAdditions.java
@@ -1,5 +1,7 @@
 package net.revna.awesomeadditions;
 
+import net.minecraft.world.item.CreativeModeTabs;
+import net.revna.awesomeadditions.item.ModItems;
 import org.slf4j.Logger;
 
 import com.mojang.logging.LogUtils;
@@ -24,17 +26,19 @@ public class AwesomeAdditions
     public static final String MOD_ID = "revnas_awesomeadditions";
     private static final Logger LOGGER = LogUtils.getLogger();
 
-    // The constructor for the mod class is the first code that is run when your mod is loaded.
-    // FML will recognize some parameter types like IEventBus or ModContainer and pass them in automatically.
+    // Mod's Constructor: runs when mod is loaded.
     public AwesomeAdditions(IEventBus modEventBus, ModContainer modContainer)
     {
         // Register the commonSetup method for modloading
         modEventBus.addListener(this::commonSetup);
 
         // Register ourselves for server and other game events we are interested in.
-        // Note that this is necessary if and only if we want *this* class (ExampleMod) to respond directly to events.
+        // Note that this is necessary if and only if we want *this* class (AwesomeAdditions) to respond directly to events.
         // Do not add this line if there are no @SubscribeEvent-annotated functions in this class, like onServerStarting() below.
         NeoForge.EVENT_BUS.register(this);
+
+        // calls the register() method so that the deferred registry of the mod's items is actually built
+        ModItems.register(modEventBus);
 
         // Register the item to a creative tab
         modEventBus.addListener(this::addCreative);
@@ -50,7 +54,11 @@ public class AwesomeAdditions
     // Add the example block item to the building blocks tab
     private void addCreative(BuildCreativeModeTabContentsEvent event)
     {
-
+        if(event.getTabKey() == CreativeModeTabs.INGREDIENTS)
+        {
+            event.accept(ModItems.FIRST_ADDITION);
+            event.accept(ModItems.SECOND_ADDITION);
+        }
     }
 
     // You can use SubscribeEvent and let the Event Bus discover methods to call
--- src/main/java/net/revna/awesomeadditions/item/ModItems.java
+++ src/main/java/net/revna/awesomeadditions/item/ModItems.java
@@ -1,6 +1,30 @@
 package net.revna.awesomeadditions.item;
 
+import net.minecraft.world.item.Item;
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.neoforge.registries.DeferredItem;
+import net.neoforged.neoforge.registries.DeferredRegister;
+import net.revna.awesomeadditions.AwesomeAdditions;
+
 public class ModItems
 {
+    public static final DeferredRegister.Items ITEMS = DeferredRegister.createItems(AwesomeAdditions.MOD_ID);
 
+    // how to add items - first item - expanded formatting for ease of seeing the different parts
+    public static final DeferredItem<Item> FIRST_ADDITION =
+            ITEMS.register // takes "item name" and Item<T> as parameters
+                    (
+                "first_addition",
+                      () -> new Item(new Item.Properties()) // "Item" takes "Item Properties" as a parameter. so scuffed
+                    );
+
+    // second item - collapsed example
+    public static final DeferredItem<Item> SECOND_ADDITION = ITEMS.register("second_addition", () -> new Item(new Item.Properties()));
+
+    // called by mod's constructor on mod startup
+    // 1. builds item registry
+    public static void register(IEventBus eventBus)
+    {
+        ITEMS.register(eventBus);
+    }
 }
